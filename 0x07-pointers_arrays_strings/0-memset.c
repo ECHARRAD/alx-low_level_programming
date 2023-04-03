c1c4abb94059de6eb1af7f878f05bd6fcd8be4a8
ECHARRAD <echarradahmed@gmail.com>
2023-04-03 15:57:52 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/* _memset - ironnou*/
+/*@x: pointer to char params*/
+/*@y: data to change*/
+/*@z: index*/
+/*Return: *x*/
+
+char *_memset(char *x, char y, unsigned int z)
+{
+	unsigned int i;
+
+	for (i = 0; i < z; i++)
+	{
+		x[i] = b;
+	}
+	return (x);
+}

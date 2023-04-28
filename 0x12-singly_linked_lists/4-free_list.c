1adefee4e4dc7310fc369fcb5bca103a448dcf2e
echarradahmed <echarradahmed@gmail.com>
2023-04-27 17:08:59 -0700

    books

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *  * free_list - frees a list_t list
+ *   * @head: head of a linked list
+ *    * Return*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}

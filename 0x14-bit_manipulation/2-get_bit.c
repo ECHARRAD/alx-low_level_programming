b45106a18349ecd005d35ac8903c8f30b9981c82
ECHARRAD <echarradahmed@gmail.com>
2023-07-12 10:24:46 -0700

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,25 +1,15 @@
-#include "main.h"
-#include <stdio.h>
-/*Write a function that returns the value of a bit at a given index.
- *
- * Prototype: int get_bit(unsigned long int n, unsigned int index);
- * where index is the index, starting from 0 of the bit you want to get
- * Returns: the value of the bit at index index or -1 if an error occured
- */
+/**
+ *  * get_bit - Returns the value of a bit at a given index
+ *   *
+ *    * @n: number to search bit in
+ *     * @index: index of the bit to return
+ *      *
+ *       * Return: Value of bit at given index or -1 if an error occured
+ *        */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-	if (n == 0 && index < 64)
-		return (0);
-
-	for (i = 0; i <= 63; n >>= 1, i++)
-	{
-		if (index == i)
-		{
-			return (n & 1);
-		}
-	}
-
-	return (-1);
+	return ((n >> index) & 1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,31 +1,18 @@
-/**
- * author: ECHARRAD AHMED
- *Write a function that sets the value of a bit to 0 at a given index.
- *Prototype: int clear_bit(unsigned long int *n, unsigned int index
- *where index is the index, starting from 0 of the bit you want to set
- *Returns: 1 if it worked, or -1 if an error occurred
- **/
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0.
- * at a given index.
- * @n: pointer of an unsigned long int.
- * @index: index of the bit.
- *
- * Return: 1 if it worked, -1 if it didn't.
- */
+ *  * clear_bit - sets the value of a bit to 0 at a given index
+ *   * @n: pointer to the number
+ *    * @index: index of the bit to clear
+ *     *
+ *      * Return: 1 if it worked, or -1 if an error occurred
+ *       */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m;
-
-	if (index > 63)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	m = 1 << index;
-
-	if (*n & m)
-		*n ^= m;
+	*n &= ~(1UL << index);
 
 	return (1);
 }

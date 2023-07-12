97778dab812f9c765198aadd13634c68718dccc3
ECHARRAD <echarradahmed@gmail.com>
2023-07-12 10:37:15 -0700

    .

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,12 @@
-/*Write a function that prints the binary representation of a number.
- *
- * Prototype: void print_binary(unsigned long int n);
- * Format: see example
- * You are not allowed to use arrays
- * You are not allowed to use malloc
- * You are not allowed to use the % or / operators
- * author: ECHARRAD AHMED
- */
 #include "main.h"
 
 /**
- *  * print_binary - prints the binary representation
- *   * of a number.
- *    * @n: unsigned long int.
- *     * author :ahmed echarrad
- *      * Return: no return.
- *       */
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ * author :ahmed echarrad
+ * Return: no return.
+ */
 void print_binary(unsigned long int n)
 {
 	if (n >> 0)
